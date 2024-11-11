2a66b62e0fca69b76d006bc66616e6198c2bd3f4
Dragon867 <hamza.jadoon2007@gmail.com>
2024-11-11 07:44:28 -0800

    Add files via upload

---
+++ Tuple into list.py
@@ -0,0 +1,9 @@
+# Sample tuple
+sample_tuple = (1, 2, 3, 4, 5)
+
+# Convert the tuple into a list
+converted_list = list(sample_tuple)
+
+# Print the result
+print("Tuple:", sample_tuple)
+print("Converted List:", converted_list)
